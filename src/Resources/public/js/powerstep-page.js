86d8169337750dfe0eb44ffe81b2fb2f376e7294
Stubbe <abs@clerk.io>
2023-06-19 11:44:23 +0200

    changed powerstep to bypass prevent default

--- src/Resources/public/js/powerstep-page.js
+++ src/Resources/public/js/powerstep-page.js
@@ -1,58 +1,77 @@
-window.onload = function() {
-        var page = $("#clerk-powerstep");
-        var buyWidget = $(".buy-widget");
-        var pageContent = $(".content-main > .container");
-        var CMSPageContent = $(".content-main > .container-main");
+const init_clerk_powerstep = (event_target) => {
+    var page = $("#clerk-powerstep");
+    var buyWidget = $(".buy-widget");
+    var pageContent = $(".content-main > .container");
+    var CMSPageContent = $(".content-main > .container-main");
 
-        buyWidget.submit(function(e) {
-            e.stopPropagation();
-            var product_Id = e.target.querySelector('input[name=product-id]').value;
-            if (e.target.querySelector('input[name=product-parentId]').value !== "") {
-                product_Id = e.target.querySelector('input[name=product-parentId]').value;
-            }
-            var product_name = e.target.querySelector('input[name=product-name').value;
-            var product_category = e.target.querySelector('input[name=product-category').value;
-            var clerk_powerstep_header = $('#clerk_powerstep_header_h2');
-            var product_image = '';
-
-            if (e.target.querySelector('input[name=product-image') !== null) {
-                product_image = e.target.querySelector('input[name=product-image').value;
-            }
-
-            var clerk_powerstep_image = $('#clerk_powerstep_image_img');
-            clerk_powerstep_header.text(product_name+' added to cart');
-            clerk_powerstep_image.attr("src", product_image);
-            clerk_powerstep_image.attr("alt", product_name);
-
-            $('.clerk-powerstep-recommendation').each(function( index ) {
-                $(this).attr('data-products','["'+product_Id+'"]');
-                $(this).attr('data-category', product_category);
-                Clerk('content','#clerk-powerstep-'+ $(this).data('template').replace('@',''));
-            });
-            pageContent.each(function( index ) {
-                $(this).hide();
-            });
-            CMSPageContent.each(function( index ) {
-                $(this).hide();
-            });
-
-            page.css("display","contents");
-            $(".offcanvas").toggle();
-            setTimeout(function(){
-                $(".modal-backdrop").toggle();
-                $('html').removeClass('no-scroll');
-            }, 100);
-
-
-        });
-
-        $(".clerk_powerstep_close").click(function () {
-            page.hide();
-            pageContent.each(function( index ) {
-                $(this).css("display","block");
-            });
-            CMSPageContent.each(function( index ) {
-                $(this).css("display","block");
-            });
-        });
+    var product_Id = event_target.querySelector('input[name=product-id]').value;
+    if (event_target.querySelector('input[name=product-parentId]').value !== "") {
+        product_Id = event_target.querySelector('input[name=product-parentId]').value;
     }
+    var product_name = event_target.querySelector('input[name=product-name').value;
+    var product_category = event_target.querySelector('input[name=product-category').value;
+    var clerk_powerstep_header = $('#clerk_powerstep_header_h2');
+    var product_image = '';
+
+    if (event_target.querySelector('input[name=product-image') !== null) {
+        product_image = event_target.querySelector('input[name=product-image').value;
+    }
+
+    var clerk_powerstep_image = $('#clerk_powerstep_image_img');
+    clerk_powerstep_header.text(product_name+' added to cart');
+    clerk_powerstep_image.attr("src", product_image);
+    clerk_powerstep_image.attr("alt", product_name);
+
+    $('.clerk-powerstep-recommendation').each(function( index ) {
+        $(this).attr('data-products','["'+product_Id+'"]');
+        $(this).attr('data-category', product_category);
+        Clerk('content','#clerk-powerstep-'+ $(this).data('template').replace('@',''));
+    });
+    pageContent.each(function( index ) {
+        $(this).hide();
+    });
+    CMSPageContent.each(function( index ) {
+        $(this).hide();
+    });
+
+    page.css("display","contents");
+    $(".offcanvas").toggle();
+    setTimeout(function(){
+        $(".modal-backdrop").toggle();
+        $('html').removeClass('no-scroll');
+    }, 100);
+
+    $(".clerk_powerstep_close").click(function () {
+        page.hide();
+        pageContent.each(function( index ) {
+            $(this).css("display","block");
+        });
+        CMSPageContent.each(function( index ) {
+            $(this).css("display","block");
+        });
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', (load_event) => {
+    const buyForms = document.querySelectorAll('form.buy-widget');
+    for(let i=0;i<buyForms.length;i++){
+        const buyForm = buyForms[i];
+        buyForm.addEventListener('submit', (submit_event) => {
+            const el = submit_event.target;
+            if(el){
+                init_clerk_powerstep(el);
+            }
+        });
+        const submitButtons = buyForm.querySelectorAll('button.btn-buy.btn-primary');
+        for(let j=0;j<submitButtons.length;j++){
+            const submitButton = submitButtons[j];
+            submitButton.addEventListener('click', (submit_event) => {
+                const el = submit_event.target.closest('form');
+                if(el){
+                    init_clerk_powerstep(el);
+                }
+            });
+        }
+    }
+});
