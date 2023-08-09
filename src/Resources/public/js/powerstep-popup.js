6ab005d88d8ed97fab9f4bd5bd6638ccfbd6d4b2
Stubbe <abs@clerk.io>
2023-08-09 17:33:55 +0200

    added gate when not finding form inputs

--- src/Resources/public/js/powerstep-popup.js
+++ src/Resources/public/js/powerstep-popup.js
@@ -1,31 +1,29 @@
 const init_clerk_powerstep = (event_target) => {
     var popup = $("#clerk-powerstep");
-    var buyWidget = $(".buy-widget");
+
     $("body").click(function() {
         if (popup.is(":visible")) {
             popup.hide();
         }
     });
-    var product_Id = event_target.querySelector('input[name=product-id]').value;
-    if (event_target.querySelector('input[name=product-parentId]').value !== "") {
-        product_Id = event_target.querySelector('input[name=product-parentId]').value;
-    }
-    var product_name = event_target.querySelector('input[name=product-name').value;
-    var product_category = event_target.querySelector('input[name=product-category').value;
+
+    const product_id_input = event_target.querySelector('input[name=product-id]') ?? null;
+    const product_parent_id_input = event_target.querySelector('input[name=product-parentId]') ?? null;
+    const product_id = product_parent_id_input?.value ?? product_id_input?.value;
+
+    const product_name = event_target.querySelector('input[name=product-name')?.value ?? '';
+    const product_category = event_target.querySelector('input[name=product-category')?.value ?? '';
+    const product_image = event_target.querySelector('input[name=product-image')?.value ?? '';
+
     var clerk_powerstep_header = $('#clerk_powerstep_header_h2');
-    var product_image = '';
-
-    if (event_target.querySelector('input[name=product-image') !== null) {
-        product_image = event_target.querySelector('input[name=product-image').value;
-    }
-
     var clerk_powerstep_image = $('#clerk_powerstep_image_img');
+
     clerk_powerstep_header.text(product_name+' added to cart');
     clerk_powerstep_image.attr("src", product_image);
     clerk_powerstep_image.attr("alt", product_name);
 
     $('.clerk-powerstep-recommendation').each(function( index ) {
-        $(this).attr('data-products','["'+product_Id+'"]');
+        $(this).attr('data-products','["'+product_id+'"]');
         $(this).attr('data-category', product_category);
         Clerk('content','#clerk-powerstep-'+ $(this).data('template').replace('@',''));
     });
