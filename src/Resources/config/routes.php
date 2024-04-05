1de70efc4152119402830b2273181e3844d8a1b5
Stubbe <abs@clerk.io>
2024-04-05 17:31:41 +0200

    changed routes to xml config

--- src/Resources/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-
-return static function (RoutingConfigurator $routes) {
-    $routes->import('../../Controller/Api/AddToCartController.php', 'annotation');
-};
